f3d45bbdba85d707f428afc034cf133369736a06
Joe Attardi <jattardi@gmail.com>
2024-06-26 22:40:06 -0400

    Add Amazon links

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -20,7 +20,10 @@
     publisher="O'Reilly Media"
     year="2024"
     href="https://learning.oreilly.com/library/view/web-api-cookbook/9781098150686/"
-  />
+    amazonHref="https://www.amazon.com/Web-API-Cookbook-JavaScript-Applications/dp/1098150694"
+  >
+    JavaScript gives web developers great power to create rich interactive browser experiences, and much of that power is provided by the browser itself. Modern web APIs enable web-based applications to come to life like never before, supporting actions that once required browser plug-ins.
+  </Book>
 
   <Book
     title="Modern CSS"
@@ -28,7 +31,10 @@
     publisher="Apress"
     year="2020"
     href="https://link.springer.com/book/10.1007/978-1-4842-6294-8"
-  />
+    amazonHref="https://www.amazon.com/Modern-CSS-Master-Concepts-Development/dp/148426293X"
+  >
+    Come on a tour of “modern” CSS in 2020. This example-driven book demonstrates the concepts by showing code examples, screenshots, and diagrams to help clearly communicate the information.
+  </Book>
 
   <Book
     title="Using Gatsby and Netlify CMS"
@@ -36,5 +42,8 @@
     publisher="Apress"
     year="2020"
     href="https://link.springer.com/book/10.1007/978-1-4842-6297-9"
-  />
+    amazonHref="https://www.amazon.com/Using-Gatsby-Netlify-CMS-JAMstack/dp/1484262964"
+  >
+    Leverage the powerful new combination of Gatsby and Netlify CMS, a free open source content management solution, to build blazing fast apps. This book shows you how to create a React-powered website using the Gatsby framework for the frontend, and Netlify CMS as the content backend. 
+  </Book>
 </div>
--- src/routes/books/Book.svelte
+++ src/routes/books/Book.svelte
@@ -4,15 +4,21 @@
   export let href;
   export let year;
   export let publisher;
+  export let amazonHref;
 </script>
 
-<div class="flex flex-col gap-4 bg-slate-200 rounded p-4">
-  <a href={href}>
-    <img 
+<div class="max-w-md flex flex-col gap-4 bg-slate-200 rounded p-4">
+  <a href={href} class="overflow-hidden">
+    <img
+      class="transition-transform duration-500 hover:-rotate- hover:scale-105"
       src={coverUrl} 
       alt={title}
     >
   </a>
-  <a href={href}><h3 class="text-2xl text-slate-700">{title}</h3></a>
+  <a href={href}><h3 class="text-2xl font-bold text-slate-700">{title}</h3></a>
   <div>{publisher}, {year}</div>
+  <a class="bg-amber-500 px-2 py-1 flex items-center gap-2 rounded self-start text-center" href={amazonHref}>
+    Get {title} on Amazon
+  </a>
+  <p><slot></slot></p>
 </div>
