243e87e094644bc1e947a856a3d5b66f55e716d6
Joe Attardi <jattardi@gmail.com>
2024-06-26 23:01:25 -0400

    Add contact icons

---
+++ src/lib/components/ContactLink.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let icon;
+  export let href;
+  export let title;
+</script>
+
+<a
+  href={href} 
+  class="text-4xl  bg-indigo-700 p-2 text-white rounded-md hover:scale-105 transition-transform"
+  title={title}
+>
+  <svelte:component this={icon} />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+  import LinkedinLogo from 'phosphor-svelte/lib/LinkedinLogo';
+  import GithubLogo from 'phosphor-svelte/lib/GithubLogo';
+  import Envelope from 'phosphor-svelte/lib/Envelope';
+  import XLogo from 'phosphor-svelte/lib/XLogo';
+  import ContactLink from '$lib/components/ContactLink.svelte';
+</script>
+
 <svelte:head>
   <title>Joe Attardi</title>
 </svelte:head>
@@ -13,6 +21,12 @@
     <p class="text-center text-2xl md:text-left md:text-3xl">
       I'm a software developer and author from the Boston area with over 20 years of experience.
     </p>
+    <div class="flex flex-row gap-4 m-auto md:m-0 md:mt-4 mt-4">
+      <ContactLink title="Email" icon={Envelope} href="mailto:jattardi@gmail.com" />
+      <ContactLink title="Twitter/X" icon={XLogo} href="https://twitter.com/JoeAttardi" />
+      <ContactLink title="GitHub" icon={GithubLogo} href="https://github.com/joeattardi" />
+      <ContactLink title="LinkedIn" icon={LinkedinLogo} href="https://linkedin.com/in/joeattardi" />
+    </div>
   </div>
 </div>
 
